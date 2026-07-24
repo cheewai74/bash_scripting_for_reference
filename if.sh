b6f5518f95ef50caf45688fc865192c9dfc32948
Chee Wai Wong (Cade) <academicsnotes@gmail.com>
2026-07-24 11:21:41 +0800

    Add files via upload

---
+++ if.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+str=""
+echo -n "Enter Your Name: "
+read str
+
+if [[ $str != "" ]]
+then
+    echo "Hello $str"
+fi
+
+num=0
+
+echo -n "Enter An Integer: "
+read num
+if [[ $num -ne  0 ]]
+then
+    echo "Received $num"
+fi
+
+if [[ $str != "" && $num -ne 0 ]]
+then 
+    echo "Thanks for $num, $str."
+fi
+
