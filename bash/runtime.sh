b89093dcda5c396e92ce7fe0614879a9a5a03072
Chee Wai Wong (Cade) <academicsnotes@gmail.com>
2026-07-24 22:49:20 +0800

    Commit scripts

---
+++ bash/runtime.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Usage: bash -x runtime.sh
+title='Bash in easy steps'
+echo $title
+echo 'End'
---
+++ bash/semantic.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# Usage: bash -v semantic.sh
+num=3
+echo $(( num * 8 + 4 ))
